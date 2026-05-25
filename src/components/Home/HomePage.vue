3002a4fc8b7c260917c6b3b214b067594c3634d1
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-25 14:41:35 +0200

    feat: Navegar a otro componente uando useRouter

--- src/components/Home/HomePage.vue
+++ src/components/Home/HomePage.vue
@@ -1,3 +1,8 @@
 <template>
   <div>Home Page</div>
+  <button @click="router.push({ name: 'contact' })">Go to Contact</button>
 </template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
