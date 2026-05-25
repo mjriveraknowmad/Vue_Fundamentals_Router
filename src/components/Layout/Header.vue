38b81b3ba8f8960f0a6fd41ace3426f3ba9c5214
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-25 15:36:36 +0200

    feat: linkActiveClass en el router, para que añada clases css en el routerlink que tenga el ":to" de la ruta actual

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -9,10 +9,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link active" to="/">Home</router-link>
+            <router-link class="nav-link" to="/">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/contact">Contact Us</router-link>
+            <router-link class="nav-link" to="/contact-us">Contact Us</router-link>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
